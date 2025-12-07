09c8da3dd40ea071d3d1bf40cd78c8d085cc92bd
NguyenHuuSang04 <24huusang6a2@gmail.com>
2025-12-07 22:06:48 +0700

    fix: Update CORS configuration to restrict allowed origins for API requests

--- src/main/java/com/notfound/bookstore/config/WebConfig.java
+++ src/main/java/com/notfound/bookstore/config/WebConfig.java
@@ -8,15 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/api/**")
-//                .allowedOrigins("http://localhost:3000")
-//                .allowedMethods("GET", "POST", "PUT", "DELETE");
-//    }
-
         registry.addMapping("/api/**")
-                .allowedOriginPatterns("*")  // ✅ Cho phép tất cả origins (để test)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(false);    // ✅ Phải set false khi dùng "*"
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
