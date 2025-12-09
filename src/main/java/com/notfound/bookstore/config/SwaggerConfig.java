9d09faef46a1c5c49ca2d290e6d37bb7a5440fc1
NguyenChiThien <22667051.thien@student.iuh.edu.vn>
2025-12-10 02:03:20 +0700

    swagger

---
+++ src/main/java/com/notfound/bookstore/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.notfound.bookstore.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Bookstore API")
+                        .version("1.0")
+                        .description("API documentation for Bookstore Management System")
+                        .contact(new Contact()
+                                .name("Bookstore Team")
+                                .email("support@bookstore.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("Local Development Server")))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("JWT token obtained from /api/auth/login")));
+    }
+}
+
